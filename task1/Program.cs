c4c8c8ef66d922b6cf909430bb30ddc81dd559d0
maxwbk <maxwbk@yandex.ru>
2023-06-21 21:54:46 +0700

    task1 ready

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int secondDigit = (num / 10) % 10;
+
+Console.WriteLine("Вторая цифра числа {0} - {1}", num, secondDigit);
+Console.ReadLine();
